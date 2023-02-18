2820e7eebb2b5abd0883ad5485c0ed807821ff5a
whoquendo <whoquendo@gmail.com>
2023-02-18 15:01:25 +0100

    correccion de ruta API para funcionamiento en modo desarrollo antes de publicar en netlify

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,3 @@
-const fs = require('fs');
-fs.writeFileSync('./.env','API=${process.env.API}\n')
+// const fs = require('fs');
+// fs.writeFileSync('./.env','API=${process.env.API}\n')
+// node ./scripts/create-env.js &&
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,11 +1,14 @@
 const API = process.env.API;
+console.log('aqui empezo el getdata');
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
+  console.log(apiURl);
   try {
     const response = await fetch(apiURl);
     const data = await response.json();
     return data.results[0];
+    
   } catch (error) {
     console.log('Fetch Error', error);
   };
