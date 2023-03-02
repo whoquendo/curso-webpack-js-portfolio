b8b38f934e143cc5f1d3254879834e86ec0823a4
whoquendo <whoquendo@gmail.com>
2023-03-02 19:57:05 +0100

    se eliminan los console logs que servieron para identificar el fix de la publicacion

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,11 +1,7 @@
 const API = process.env.API;
 
-
-console.log('aqui empezo el getdata');
-
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
-  console.log(apiURl);
   try {
     const response = await fetch(apiURl);
     const data = await response.json();
